06df6a4f95b22824a15ffbd0296ee43b74eead0a
Casper Neo <casperneo@uchicago.edu>
2018-08-06 13:52:05 -0500

    analysis now puts analysis image in model directory, updated model to be fully convolutional

--- dnn/analysis.py
+++ dnn/analysis.py
@@ -2,6 +2,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
 from scipy.cluster.vq import kmeans
+import tensorflow as tf
+import pipeline
 
 import matplotlib
 
@@ -22,8 +24,8 @@ def plot_ae_output(dataset, predictions, n_samples, n_bands, height=4, width=4):
     for i in range(n_samples):
         for j in range(n_bands):
             orig, pred = ax[i * 2 : i * 2 + 2, j]
-            orig.imshow(dataset[i, :, :, j], cmap="binary")
-            pred.imshow(predictions[i, :, :, j], cmap="binary")
+            orig.imshow(dataset[i, :, :, j], cmap="copper")
+            pred.imshow(predictions[i, :, :, j], cmap="copper")
             for a in (orig, pred):
                 a.set_xlabel("band %d" % (j + 1))
                 a.axis("off")
@@ -53,7 +55,7 @@ def img_scatter(points, images, zoom=0.5):
     fig, ax = plt.subplots(figsize=(20, 20))
 
     for ((x, y), img) in zip(points, images):
-        im = OffsetImage(img, zoom=zoom, cmap="binary")
+        im = OffsetImage(img, zoom=zoom, cmap="copper")
         ab = AnnotationBbox(im, (x, y), xycoords="data", frameon=False)
         ax.add_artist(ab)
         ax.update_datalim(np.column_stack([x, y]))
@@ -62,17 +64,20 @@ def img_scatter(points, images, zoom=0.5):
     return fig, ax
 
 
-def plot_kmeans_and_image_catter(original, encoded, K=10):
+def plot_kmeans_and_image_scatter(original, encoded, K=3):
     """Plots AE encoded space projected down by PCA with scattered images and
     k means.
     """
     # Spatial average features
-    centered = encoded - encoded.mean(axis=(1, 2))
+    centered = encoded.mean(axis=(1, 2))
+    centered = centered - centered.mean(axis=0)
     pc = PCA(centered)
     proj = pc.project(centered, 2)
 
     # Average over all the bands for plotting purposes
-    fig, ax = analysis.img_scatter(proj, original.mean(axis=3), zoom=0.5)
+    imgs = original.mean(axis=3)
+    fig, ax = img_scatter(proj, imgs, zoom=0.5)
+    ax.set_title("Autoencoder hidden space PCA projection")
 
     # Add K Means cluster centers projected down
     codebook, distortion = kmeans(centered, K)
@@ -80,3 +85,63 @@ def plot_kmeans_and_image_catter(original, encoded, K=10):
     ax.scatter(xs, ys, s=100, c="r", zorder=1000)
 
     return fig, ax
+
+
+if __name__ == "__main__":
+    import argparse
+
+    # TODO Dont repeat common loading stuff in this section and in train.py
+    p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    p.add_argument("data", help="/path/to/data.tif")
+    p.add_argument(
+        "-sh",
+        "--shape",
+        nargs=3,
+        type=int,
+        help="Shape of input image",
+        default=(64, 64, 7),
+    )
+    p.add_argument(
+        "model_dir", help="/path/to/model/ to load model from and to save inference"
+    )
+    p.add_argument("-ns", "--num_samples", type=int, help=" ", default=1000)
+
+    FLAGS = p.parse_args()
+    if FLAGS.model_dir[-1] != "/":
+        FLAGS.model_dir += "/"
+
+    ae = tf.keras.models.load_model(FLAGS.model_dir + "model.h5")
+
+    img_width, img_height, n_bands = FLAGS.shape
+    del img_height  # Unused TODO maybe use it?
+    x = (
+        tf.data.Dataset.from_generator(
+            pipeline.read_tiff_gen([FLAGS.data], img_width),
+            tf.float32,
+            (img_width, img_width, n_bands),
+        )
+        .apply(tf.contrib.data.shuffle_and_repeat(100))
+        .batch(FLAGS.num_samples)
+        .make_one_shot_iterator()
+        .get_next()
+    )
+
+    with tf.Session() as sess:
+        x = sess.run(x)
+
+    # TODO how to pick hidden layer?
+    en = tf.keras.models.Model(inputs=ae.input, outputs=ae.get_layer("conv2d_3").output)
+    e = en.predict(x)
+    y = ae.predict(x)
+
+    # Save autoencoder output
+    plot_ae_output(x, y, 2, n_bands)
+    fname = FLAGS.model_dir + "ae_output.png"
+    plt.savefig(fname)
+    print(f"Autoencoder Results saved to {fname}")
+
+    # Save Hidden space analysis
+    fig, _ = plot_kmeans_and_image_scatter(x, e)
+    fname = FLAGS.model_dir + "pca.png"
+    fig.savefig(fname)
+    print(f"Hidden space diagram saved to {fname}")
