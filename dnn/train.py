6c77b03f0c4da6545760ef81ff5225d151d60295
caspern <caspern@uchicago.edu>
2018-08-09 08:14:23 -0500

    midway push stuff from when we discovered band error in tfr

--- dnn/train.py
+++ dnn/train.py
@@ -9,7 +9,7 @@ import os
 
 # Parse Arguments
 p = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-p.add_argument("data", help="/path/to/data.tiff")
+p.add_argument("data_glob", help="pattern to pick up tf records files")
 p.add_argument(
     "model_dir",
     help="/path/to/model/ to load and train or to save new model",
@@ -54,12 +54,12 @@ features = {
 }
 
 def stack_bands(x):
-    tf.stack([x[f"b{i+1}"] for i in range(n_bands)])
+    return tf.stack([x[f"b{i+1}"] for i in range(n_bands)], axis=2)
 
 
 data = (
-    tf.data.TFRecordDataset(FLAGS.data)
-    .apply(tf.contrib.data.shuffle_and_repeat(500))
+    tf.data.Dataset.list_files(FLAGS.data_glob)  
+    .flat_map(tf.data.TFRecordDataset)
     .map(lambda serialized: tf.parse_single_example(serialized, features))
     .map(stack_bands)
     .batch(FLAGS.batch_size)
