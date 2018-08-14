e5a08b34bf037800136b80c5e897bbe418987732
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2018-08-14 17:00:54 -0500

    Generating JSON file with metadata. Still missing to load HDF binaries on the TFRecord object.

--- nasa_laads/hd5_to_TFRecord.py
+++ nasa_laads/hd5_to_TFRecord.py
@@ -9,8 +9,8 @@ import json
 import numpy as np
 
 
-def HDFtoTFRecord(url_folder,file_name,file_extension):
-    fileURL = url_folder+file_name+file_extension
+def HDFtoTFRecord(url_folder, file_name, file_extension):
+    fileURL = url_folder + file_name + file_extension
     # Get file and create pyHDF object
     HDFobj = SD(fileURL, SDC.READ)
 
@@ -20,9 +20,10 @@ def HDFtoTFRecord(url_folder,file_name,file_extension):
     datafields = list()
     shapelist = list()
 
-    tfrecords_filename = file_name+'.TFRecord'
+    tfrecords_filename = file_name + '.TFRecord'
     writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
+    json_buffer = {file_name: {}}
     record_dict = {}
     for idx, sds in enumerate(datasets_dic.keys()):
 
@@ -32,41 +33,75 @@ def HDFtoTFRecord(url_folder,file_name,file_extension):
         shapelist.append(singlefield[:].shape)
 
         if len(shapelist[idx]) == 1:
-            tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]), 1,1,None)
+            parameters = datafields[idx], max(shapelist[idx]), 1, 1, None
+            tf_dict = dimension_dict(parameters)
+            js_dict = json_dict(parameters)
 
         if len(shapelist[idx]) == 2:
-            tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]),min(shapelist[idx]),1,None)
+            parameters = datafields[idx], max(shapelist[idx]), min(shapelist[idx]), 1, None
+            tf_dict = dimension_dict(parameters)
+            js_dict = json_dict(parameters)
 
         elif len(shapelist[idx]) == 3:
-            tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]),median(shapelist[idx]),min(shapelist[idx]),None)
+            parameters = datafields[idx], max(shapelist[idx]), median(shapelist[idx]), min(shapelist[idx]), None
+            tf_dict = dimension_dict(parameters)
+            js_dict = json_dict(parameters)
 
-
-
+        # Transient memory
         transient_record_dict = record_dict
         transient_record_dict.update(tf_dict)
         record_dict = transient_record_dict
-    print(record_dict)
+
+        transient_js_dict = json_buffer[file_name]
+        transient_js_dict.update(js_dict)
+        json_buffer[file_name] = transient_js_dict
+        ###################
+
+    # print(record_dict)
+    print(json_buffer)
     features_TF_obj = tf.train.Features(feature=record_dict)
     example = tf.train.Example(features=features_TF_obj)
     writer.write(example.SerializeToString())
+
+    # dump json object
+    with open(file_name+'.json', 'w') as outfile:
+        json.dump(json_buffer, outfile, ensure_ascii=False)
+
     return
 
-def dimension_dict(hdffield, height, width, channels, binaryvalues):
-    dictionary =  {
-                      hdffield+'_height': int64_feature(height),
-                      hdffield+'_width': int64_feature(width),
-                      hdffield+'_channels': int64_feature(channels),
-                      hdffield+'_binary_raw': bytes_feature(np.zeros(1).tobytes())
-                  }
+
+def dimension_dict(tuple):
+    hdffield=tuple[0]
+    binaryvalues=tuple[4]
+    dictionary = {
+        hdffield + '_binary_raw': bytes_feature(np.zeros(1).tobytes())
+    }
 
     return dictionary
+
+
+def json_dict(tuple):
+    hdffield = tuple[0]
+    height = tuple[1]
+    width = tuple[2]
+    channels = tuple[3]
+    dictionary = {
+        hdffield + '_height': height,
+        hdffield + '_width': width,
+        hdffield + '_channels': channels
+    }
+    return dictionary
+
 
 # TF Helper functions
 def bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
+
 def int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
+
+
 ##################
 
 # Testing routine
