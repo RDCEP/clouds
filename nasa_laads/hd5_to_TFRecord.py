cf930008cff02bb02480ea84ae9ec3cbf300b1ac
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2018-08-14 15:35:39 -0500

    Now working. Moving to dump actual binary values.

--- nasa_laads/hd5_to_TFRecord.py
+++ nasa_laads/hd5_to_TFRecord.py
@@ -23,65 +23,42 @@ def HDFtoTFRecord(url_folder,file_name,file_extension):
     tfrecords_filename = file_name+'.TFRecord'
     writer = tf.python_io.TFRecordWriter(tfrecords_filename)
 
-    record_dict = {file_name: {}}
+    record_dict = {}
     for idx, sds in enumerate(datasets_dic.keys()):
-        # print(sds)#idx, sds)
+
         datafields.append(sds)
 
         singlefield = HDFobj.select(sds)
         shapelist.append(singlefield[:].shape)
 
-        print(idx)
-        # print(datafields[idx])
-        # print(shapelist[idx])
-
-        # print(dimension_dict(datafields[idx],))
         if len(shapelist[idx]) == 1:
             tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]), 1,1,None)
-            # print(tf_dict)
+
         if len(shapelist[idx]) == 2:
             tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]),min(shapelist[idx]),1,None)
-            # print(tf_dict)
+
         elif len(shapelist[idx]) == 3:
             tf_dict = dimension_dict(datafields[idx],max(shapelist[idx]),median(shapelist[idx]),min(shapelist[idx]),None)
-            # print(tf_dict)
-        # else:
-            # print('Error: HDF dimension with unknown datastructure')
-            # print('Tuple length: '+str(len(shapelist[idx])))
-            # print(datafields[idx])
-            # print(shapelist[idx])
-            # return
 
-        # record_dict[file_name].append(tf_dict)
-        transient_record_dict = record_dict[file_name]
+
+
+        transient_record_dict = record_dict
         transient_record_dict.update(tf_dict)
-        record_dict[file_name] = transient_record_dict
-
-    # For every data element in list return its shapes
-
-    # print(datafields[15])
-    # print(shapelist[15])
-    # print(len(shapelist[15]))
-    # print(shapelist[15][0])
-    # print(shapelist[15][1])
-    # print(shapelist[15][2])
-    # print(min(shapelist[15]))
-    # for key in datafields:
-
-    # print(json.dumps(record_dict, ensure_ascii=False))
+        record_dict = transient_record_dict
+    print(record_dict)
     features_TF_obj = tf.train.Features(feature=record_dict)
-    example = tf.train.SequenceExample(features=features_TF_obj)
+    example = tf.train.Example(features=features_TF_obj)
     writer.write(example.SerializeToString())
     return
 
 def dimension_dict(hdffield, height, width, channels, binaryvalues):
-    dictionary = {hdffield : {
-                      'height': int64_feature(height),
-                      'width': int64_feature(width),
-                      'channels': int64_feature(channels),
-                      'binary_raw': bytes_feature(np.zeros(1).tobytes())
+    dictionary =  {
+                      hdffield+'_height': int64_feature(height),
+                      hdffield+'_width': int64_feature(width),
+                      hdffield+'_channels': int64_feature(channels),
+                      hdffield+'_binary_raw': bytes_feature(np.zeros(1).tobytes())
                   }
-    }
+
     return dictionary
 
 # TF Helper functions
