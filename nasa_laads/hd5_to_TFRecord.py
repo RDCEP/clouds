34f0857a918c95837a0be8653d55dcb5d1a4e7e8
Casper Neo <casperneo@uchicago.edu>
2018-08-14 14:22:27 -0500

    cnn per trains on my computer, uncompressed tif too big for tfrecord

--- nasa_laads/hd5_to_TFRecord.py
+++ nasa_laads/hd5_to_TFRecord.py
@@ -2,6 +2,5 @@ import tensorflow as tf
 
 from pyhdf.SD import SD, SDC
 
-file_name = 'MOD06_L2.A2017001.0115.061.2017312163804.hd5'
+file_name = "MOD06_L2.A2017001.0115.061.2017312163804.hd5"
 file = SD(file_name, SDC.READ)
-
