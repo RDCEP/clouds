8226785e2a0569681095bcf93c0266b58c91a92b
Casper Neo <casperneo@uchicago.edu>
2018-08-21 15:41:28 -0500

    profiling

--- test.sh
+++ test.sh
@@ -5,12 +5,17 @@ HDF_DATA=$DATA_DIR/ex.tfrecord
 HDF_META=$DATA_DIR/ex.json
 FIELDS='Cloud_Optical_Thickness Cloud_Effective_Radius'
 
+
+rm -rf $MODEL_PATH
+
 python dnn/train2.py $MODEL_PATH \
     --data $HDF_DATA \
     --hdf_fields $FIELDS \
     --meta_json $HDF_META \
-    --epochs 500 \
-    --n_layers 5 \
+    --epochs 1 \
+    --steps_per_epoch 100 \
+    --summary_every 25 \
+    --n_layers 2 \
     --red_bands 0 \
     --blue_bands 0 \
     --green_bands 0 \
