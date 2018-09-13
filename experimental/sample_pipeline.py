f30a7c9a859b445abe7eeed36ca9b8b532773ae5
Casper Neo <casperneo@uchicago.edu>
2018-09-13 12:36:31 -0500

    moved some plotting from Analysis to sample_pipeline, updated Analysis loading

--- experimental/sample_pipeline.py
+++ experimental/sample_pipeline.py
@@ -1,11 +1,13 @@
 import sys
-sys.path.insert(0, "..")
-import tensorflow as tf
-from reproduction.pipeline.load import add_pipeline_cli_arguments, load_data
 import os
 import numpy as np
+import tensorflow as tf
 from matplotlib import pyplot as plt
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+sys.path.insert(1, os.path.join(sys.path[0], ".."))
+from reproduction.pipeline.load import add_pipeline_cli_arguments, load_data
+
 
 def get_args():
     p = ArgumentParser(
@@ -18,7 +20,18 @@ def get_args():
     return p.parse_args()
 
 
-def save_samples(fields, samples, fname, width=3):
+def sample_dataset(dataset, n):
+    batch = dataset.make_one_shot_iterator().get_next()
+    samples = []
+    with tf.Session() as sess:
+        while len(samples) < n:
+            names, coords, imgs = sess.run(batch)
+            samples.extend(list(zip(names, zip(*coords), imgs)))
+    samples = samples[:n]
+    return samples
+
+
+def plot_samples(samples, fields, width=3):
     fig, ax = plt.subplots(
         nrows=len(samples),
         ncols=len(fields),
@@ -28,16 +41,19 @@ def save_samples(fields, samples, fname, width=3):
         for col, field in enumerate(fields):
             a = ax[row, col]
             a.imshow(img[:, :, col], cmap="bone")
-            a.set_axis_off()
+            # a.set_axis_off()
+            a.set_xticks([])
+            a.set_yticks([])
             if row == 0:
                 a.set_title(field)
-            if row and col == 0:
-                a.set_title("{}:{}".format(f, coord))
+            if row and col == 2:
+                name = os.path.basename(str(f))
+                name = os.path.splitext(name)[0]
+                a.set_title("{}:{}".format(name, coord), fontsize=10)
+    return fig
 
-    fig.savefig(fname)
 
-
-def save_hists(fields, samples, fname):
+def plot_hists(samples, fields):
     samples = np.stack([img for _, _, img in samples])
     fig, ax = plt.subplots(ncols=len(fields), figsize=(len(fields) * 5, 5))
 
@@ -45,7 +61,7 @@ def save_hists(fields, samples, fname):
         a = ax[i]
         a.hist(samples[:, :, :, i].ravel(), density=True)
         a.set_title(f)
-    fig.savefig(fname)
+    return fig
 
 
 if __name__ == "__main__":
@@ -64,17 +80,13 @@ if __name__ == "__main__":
     )
     shape = (*FLAGS.shape, chans)
 
-    batch = dataset.make_one_shot_iterator().get_next()
-
-    # Get samples
-    samples = []
-    with tf.Session() as sess:
-        while len(samples) < FLAGS.n_samples:
-            names, coords, imgs = sess.run(batch)
-            samples.extend(list(zip(names, zip(*coords), imgs)))
-    samples = samples[: FLAGS.n_samples]
+    sample_dataset(dataset, FLAGS.n_samples)
 
     # Save figs
     os.makedirs(FLAGS.output_dir, exist_ok=True)
-    save_samples(FLAGS.fields, samples, os.path.join(FLAGS.output_dir, "samples.png"))
-    save_hists(FLAGS.fields, samples, os.path.join(FLAGS.output_dir, "hists.png"))
+
+    fig = plot_samples(samples, FLAGS.fields)
+    fig.savefig(os.path.join(FLAGS.output_dir, "samples.png"))
+
+    fig = plot_hists(samples, FLAGS.fields)
+    fig.savefig(os.path.join(FLAGS.output_dir, "hists.png"))
