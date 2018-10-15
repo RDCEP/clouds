4b2b836381cf5813f688f119a1cbb9da332ab1aa
Ricardo Barros Lourenco <ricardobarroslourenco@gmail.com>
2018-10-15 10:54:23 -0500

    Skeleton for mod021km...

--- reproduction/pipeline/into_record.py
+++ reproduction/pipeline/into_record.py
@@ -8,6 +8,8 @@ import numpy as np
 from osgeo import gdal
 from mpi4py import MPI
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+from os import path
+from pyhdf.SD import SD, SDC
 
 
 def _int64_feature(value):
@@ -162,6 +164,75 @@ def get_args(verbose=False):
     FLAGS.out_dir = os.path.abspath(FLAGS.out_dir)
     return FLAGS
 
+def hdf2tfr(hdf_file, out_dir, target_fields):
+    """Converts HDF file into a tf record by serializing all fields and	
+    names to a record. Also outputs a json file holding the meta data.	
+    Arguments:	
+        hdf_file: File to convert into a tf record	
+        target_fields: List of specified fields to convert	
+    """
+
+    file_name = input_folder + 'MOD021KM.A2017001.2330.006.2017004135453.hdf'
+    file = SD(file_name, SDC.READ)
+
+    names = (
+        "EV_250_Aggr1km_RefSB",
+        "EV_500_Aggr1km_RefSB",
+        "EV_1KM_RefSB",
+        "EV_1KM_Emissive"
+    )
+
+    fields = [file.select(n)[:] for n in names]
+
+    labels = [["b1", "b2"],
+              ["b3", "b4", "b5", "b6", "b7"],
+              ["b8", "b9", "b10", "b11", "b12", "b13L", "b13H", "b14L", "b14H", "b15", "b16", "b17", "b18", "b19", "b26"],
+              ["b20", "b21", "b22", "b23", "b24", "b25", "b27", "b28", "b29", "b30", "b31", "b32", "b33", "b34", "b35",
+              "b36"]]
+
+    res = np.stack([
+        fields[0],
+        fields[1],
+        fields[2][??],
+    fields[2][???],
+    fields[3][:5]
+    ], axis = 0)
+
+
+    yield fname, (x,y), patch
+
+    for n in names:
+        print(
+            file.select(n)[:].shape)
+
+    # hdf = SD(hdf_file, SDC.READ)
+    # meta = {}
+    # features = {}
+    # for field in hdf.datasets().keys():
+    #     if target_fields and field not in target_fields:
+    #         continue
+    #      data = hdf.select(field)[:]  # Index to get it as np array
+    #      # Make sure every field is of rank 4
+    #     while len(data.shape) < 3:
+    #         data = np.expand_dims(data, -1)
+    #     if len(data.shape) > 3:
+    #         print(
+    #             "Warning, encountered high rank field %s with shape %s"
+    #             % (field, data.shape)
+    #         )
+    #         continue
+    #      # Reorder dimensions such that it is longest dimension first
+    #     # Assumes height > width > channels
+    #     rank_order = list(np.argsort(data.shape))
+    #     rank_order.reverse()
+    #     data = data.transpose(rank_order)
+    #      ty = str(data.dtype)
+    #     meta[field] = [data.shape[-1], ty]
+    #     features[field + "/shape"] = _int64_feature(data.shape)
+    #     features[field + "/type"] = _bytes_feature(ty.encode("utf_8"))
+    #     features[field] = _bytes_feature(data.tobytes())
+    #  out_file = path.join(out_dir, path.basename(hdf_file))
+    #  save_example_and_metadata(out_file, [features], meta)
 
 if __name__ == "__main__":
     comm = MPI.COMM_WORLD
