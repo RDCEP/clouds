45c3e521a28a2d82b003979c225a912de8106caf
caspern <caspern@uchicago.edu>
2018-11-06 15:15:48 -0600

    midway changes

--- reproduction/clustermodels.py
+++ reproduction/clustermodels.py
@@ -60,25 +60,23 @@ def get_latest(model_dir, model):
         step = int(s.split("-")[1].split(".joblib")[0])
         return step, joblib.load(s)
 
-    return 0, None
+    if model == "sparse_dict":
+        model = MiniBatchDictionaryLearning(FLAGS.n_clusters)
 
+    elif model == "kmeans":
+        model = MiniBatchKMeans(FLAGS.n_clusters)
+
+    else:
+        raise ValueError("Invalid model", FLAGS.model)
+
+    return 0, model
 
 step, model = get_latest(FLAGS.out_dir, FLAGS.model)
-
-if model is None and FLAGS.model == "sparse_dict":
-    model = MiniBatchDictionaryLearning(FLAGS.n_clusters)
-
-elif FLAGS.model == "kmeans":
-    model = MiniBatchKMeans(FLAGS.n_clusters)
-
-else:
-    raise ValueError("Invalid model", FLAGS.model)
-
 
 # Start training
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    encoder.load_weights(os.path.join(FLAGS.encoder, "encoder.h5"))
+    encoder.load_weights(os.path.join(FLAGS.encoder, "encoder-"+FLAGS.encoder_step+".h5"))
 
     for step in range(step, FLAGS.max_steps):
         # c = sess.run(codes)
@@ -88,7 +86,7 @@ with tf.Session() as sess:
         model.partial_fit(c)
 
         if step % FLAGS.summary_every == 0:
-            pass  # TODO
+            print("Step", step, flush=True)
 
         if step % FLAGS.save_every == 0:
             seen = step * FLAGS.batch_size
