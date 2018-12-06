44e5e0be5f527f3c9a64fc4d6c7ae276b8e5820d
ricardobarroslourenco <ricardobarroslourenco@gmail.com>
2018-12-06 01:05:52 -0600

    Passing parameters

--- analysis/2018agu_fall/3-clustering-agglomerative-parallel_grid_search.py
+++ analysis/2018agu_fall/3-clustering-agglomerative-parallel_grid_search.py
@@ -14,17 +14,20 @@ import multiprocessing as mp
 from itertools import product
 
 # Disable Warnings
-import warnings; warnings.simplefilter('ignore')
+import warnings;
+
+warnings.simplefilter('ignore')
 tf.logging.set_verbosity(tf.logging.WARN)
 
-def gridsearch(start, step, stop, max_samples=5000, sample_steps=4, trials=30):
-    with open(ENCODER_DEF,"r") as f:
-            encoder = tf.keras.models.model_from_json(f.read())
-    encoder.load_weights(ENCODER_WEIGHTS)
-    
-    samples = np.logspace(np.log10(start+2), np.log10(max_samples), num=sample_steps).astype(int)
 
-    def trial_test(i, j):
+def gridsearch(start, step, stop, max_samples=5000, sample_steps=4, trials=30):
+    with open(ENCODER_DEF, "r") as f:
+        encoder = tf.keras.models.model_from_json(f.read())
+    encoder.load_weights(ENCODER_WEIGHTS)
+
+    samples = np.logspace(np.log10(start + 2), np.log10(max_samples), num=sample_steps).astype(int)
+
+    def trial_test(i, j, samples, encoder):
         search_results = []  # Force initialization
         print('Samples: ', i, ' Clusters: ', j)
         minfoac = []
@@ -44,17 +47,16 @@ def gridsearch(start, step, stop, max_samples=5000, sample_steps=4, trials=30):
 
     print(list(product(samples, range(start, stop, step))), flush=True)
     with mp.Pool(processes=8) as pool:
-        results = pool.starmap(trial_test, product(samples, range(start, stop, step)))
+        results = pool.starmap(trial_test, product(samples, range(start, stop, step), samples, encoder))
 
     return results
-    
+
 
 DATA = "/project/foster/clouds/data/2015_05/*.tfrecord"
 ENCODER_DEF = "/home/rlourenco/rdcep_clouds/output/m9-22_oceans/encoder.json"
 ENCODER_WEIGHTS = "/home/rlourenco/rdcep_clouds/output/m9-22_oceans/encoder.h5"
 
+result = gridsearch(2, 1, 40, max_samples=10000, sample_steps=10)
 
-result = gridsearch(2,1,40,max_samples=10000, sample_steps=10)
-
-df_export = pd.DataFrame.from_records(result, columns=['Samples','Clusters','Avg_MInfo','Std_MInfo'])
+df_export = pd.DataFrame.from_records(result, columns=['Samples', 'Clusters', 'Avg_MInfo', 'Std_MInfo'])
 df_export.to_csv('searched_results.csv', encoding='utf-8', index=False)
