2e41054bacecde8de707b647f5ade6786f8ad4f4
Katy Koenig <koenig1@cs.uchicago.edu>
2019-06-19 16:24:53 -0500

    Pushing the bash script I'm running b/c downloading lighter files than usual

--- src_analysis/load_hdfs/200_geturl.bash
+++ src_analysis/load_hdfs/200_geturl.bash
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-python prg_geturl.py \
-   --datedata='/home/tkurihana/src/src_metadata/c0_.txt'\
-   --outputdir=/home/tkurihana/scratch-midway2/data/MOD02/clustering_laads_2000_2018_5 \
-   --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD021KM" \
-   --keyward='MOD021KM.A' \
-   --thresval=100 
+python3 prg_geturl.py \
+   --datedata='/home/koenig1/clouds/src_analysis/metadata/output.txt'\
+   --outputdir='/home/koenig1/clouds/src_analysis/load_hdfs/test' \
+   --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2" \
+   --keyward='MOD35_L2.A' \
+   --thresval=1 \
+   --processor=1
 
    #--datedata='/home/tkurihana/src/src_metadata/c0.txt'\
 ## past parser
