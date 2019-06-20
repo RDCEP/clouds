bbee9a025a925ab650c0f27c74c2d8ad6f0dd09b
Katy Koenig <koenig1@cs.uchicago.edu>
2019-06-20 16:58:16 -0500

    typo fixed

--- src_analysis/combined/download.bash
+++ src_analysis/combined/download.bash
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-python3 prg_geturl2.py --days 1 \
+python3 prg_geturl2.py --days=1 \
    --start='2000-02-24'\
    --end='2019-03-27'\
    --outputdir='/home/koenig1/clouds/src_analysis/combined/test' \
    --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2" \
    --keyward='MOD35_L2.A' \
    --thresval=1 \
-   --processors=7 \
+   --processors=4 \
    --datedata='/home/koenig1/clouds/src_analysis/combined/output.txt'\
