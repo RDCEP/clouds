dded92d1a9023e567ea0778f970547612a5dadc9
Katy Koenig <kkoenig810@gmail.com>
2019-06-28 16:07:42 -0500

    changed name of date file

--- src_analysis/api-requests/api_request.py
+++ src_analysis/api-requests/api_request.py
@@ -20,7 +20,7 @@ import laads_data_download as ldd
 EMAIL = ''
 APP_KEY = ''
 ########################################################
-DATE_FILE = 'one_date.txt'
+DATE_FILE = 'label1.txt'
 COORDINATES_FILE = 'coords.csv'
 
 ### Function to make csv of coordinates for patches ####
@@ -39,7 +39,7 @@ def write_csv(outputfile='coords.csv'):
         outputwriter = csv.writer(csvfile, delimiter=',')
         outputwriter.writerow(['name', 'north', 'south', 'east', 'west'])
         outputwriter.writerow(['open_pacific', 32.5, 12.4, 127.7, -155.4])
-        outputwriter.writerow(['open_south_sf', -34.3, -49.9, 40.2, 23.5])
+        #outputwriter.writerow(['open_south_sf', -34.3, -49.9, 40.2, 23.5])
         # outputwriter.writerow(['closed_west_sf', -19.6, -44.9, 14.2, -5.6])
         # outputwriter.writerow(['open_west_atlantic', 42, 23.6, -48.1, -74.7])
         # outputwriter.writerow(['closed_east_atlantic', 33.6, 12.4, -15.9, -37.5])
