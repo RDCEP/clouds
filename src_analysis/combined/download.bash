a22cf383ebfdd550312124bb2638356da7df2a35
Katy Koenig <koenig1@cs.uchicago.edu>
2019-07-08 10:55:02 -0500

    Edited path and desired dates for midway download

--- src_analysis/combined/download.bash
+++ src_analysis/combined/download.bash
@@ -3,9 +3,10 @@
 python3 prg_geturl2.py --days=1 \
    --start='2000-02-24'\
    --end='2019-03-27'\
-   --outputdir='/home/koenig1/clouds/src_analysis/combined/test' \
+   --outputdir='MOD35/clustering/' \
    --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2" \
    --keyward='MOD35_L2.A' \
    --thresval=1 \
-   --processors=4 \
-   --datedata='/home/koenig1/clouds/src_analysis/combined/output.txt'\
+   --processors=7 \
+   --datedata='mod02_training_dates.txt'\
+
