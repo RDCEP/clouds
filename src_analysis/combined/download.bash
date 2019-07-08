f80b610cd3ed861e49f41410c1fc478b8fd3391d
Katy Koenig <koenig1@cs.uchicago.edu>
2019-07-08 11:05:35 -0500

    edited output dir

--- src_analysis/combined/download.bash
+++ src_analysis/combined/download.bash
@@ -3,10 +3,10 @@
 python3 prg_geturl2.py --days=1 \
    --start='2000-02-24'\
    --end='2019-03-27'\
-   --outputdir='MOD35/clustering/' \
+   --outputdir='/home/koenig1/scratch-midway2/MOD35/clustering/specific_dates' \
    --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2" \
    --keyward='MOD35_L2.A' \
    --thresval=1 \
    --processors=7 \
-   --datedata='mod02_training_dates.txt'\
+   --datedata='clustering_mod35_list.txt'\
 
