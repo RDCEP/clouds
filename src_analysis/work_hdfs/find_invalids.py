abd30873dd26c81bdd1c7e0147ecee9ec13dee73
Katy Koenig <koenig1@cs.uchicago.edu>
2019-07-08 15:29:53 -0500

    edited to link desired files

--- src_analysis/work_hdfs/find_invalids.py
+++ src_analysis/work_hdfs/find_invalids.py
@@ -7,37 +7,30 @@ Functions to check for invalid hdf files
 '''
 
 import os
-import sys
-import glob
-import numpy as np
-import matplotlib.pyplot as plt
-from pyhdf.SD import SD, SDC
-from alignment_lib import gen_mod02_img_sigle, gen_mod35_img_single
-from alignment_lib import mod02_proc_sds_single
-from alignment_lib import _gen_patches
-from alignment_lib import const_clouds_array
 
 
 DATES_FILE = 'clustering_invalid_filelists.txt'
 HDF_DIRECTORY = '/home/koenig1/scratch-midway2/MOD02/clustering'
+DEST_DIRECTORY = '/home/koenig1/scratch-midway2/distribution'
 
-def get_dates(filename=DATES_FILE, directory=HDF_DIRECTORY):
+def get_dates(filename=DATES_FILE, directory=HDF_DIRECTORY, destination=DEST_DIRECTORY):
     '''
+    Searches for desired files and links them to destination directory to be called later
 
     Inputs:
         filename:
+        directory:
+        destination:
 
     Outputs:
+        None
     '''
     with open(filename, "r") as file:
         dates = file.readlines()
     desired_files = dates[0].replace('hdf', 'hdf ').split()
 
     for path, directories, files in os.walk(directory):
-        hdf_lst = [x for x in files]
-
-
-    for date in desired_files:
-        os.listdir(directory)
-
+        for file in files:
+            if file in desired_files:
+                os.link(path + "/" + file, destination)
 
