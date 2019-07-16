50a918f398500fc5193dd9df00405d0998df2884
Sydney Jenkins <sydneyjenkins@midway2-login1.rcc.local>
2019-07-16 14:52:31 -0500

    changes

--- src_analysis/combined/download.bash
+++ src_analysis/combined/download.bash
@@ -3,10 +3,10 @@
 python3 prg_geturl2.py --days=1 \
    --start='2000-02-24'\
    --end='2019-03-27'\
-   --outputdir='/home/koenig1/scratch-midway2/MOD35/clustering/specific_dates' \
+   --outputdir='/home/sydneyjenkins/scratch-midway2/MOD35_data' \
    --url="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MOD35_L2" \
    --keyward='MOD35_L2.A' \
    --thresval=1 \
    --processors=7 \
-   --datedata='clustering_mod35_list.txt'\
+   --datedata='/home/sydneyjenkins/scratch-midway2/clouds/randomforest_laads_1.txt'\
 
