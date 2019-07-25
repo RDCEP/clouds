61a7338307f0c9146eff46a765f25196c055325f
TakGlobus <42143716+TakGlobus@users.noreply.github.com>
2019-07-24 19:18:23 -0500

    07/24 finish global mean&std MPI program

--- reproduction/pipeline/test_mpi_globalMeanStd.py
+++ reproduction/pipeline/test_mpi_globalMeanStd.py
@@ -50,10 +50,13 @@ from alignment_lib import gen_mod02_img, gen_mod35_img
 from alignment_lib import const_clouds_array
 from alignment_lib import _gen_patches
 from alignment_lib import get_filepath
-
+from lib_modis02   import load_tfrecord
+from lib_modis02   import _get_imgs
+from lib_modis02   import _get_num_imgs
 
 def compute_global_single(mod02_datadir='./', mod35_datadir='./', 
                           thres=0.3, nx=128, ny=128, nbands=6):
+  #TODO: add if & lines to read tfrecord files
   mod02_filelist = glob.glob(mod02_datadir+'/*.hdf')
   clouds_patches_list = []
   for m2_file in mod02_filelist:
@@ -81,42 +84,89 @@ def compute_global_single(mod02_datadir='./', mod35_datadir='./',
   return gmean, gstdv
 
 
-def compute_local(m2_filelist='./', mod35_datadir='./', 
+def compute_local(m2_filelist=[], 
+                 mod35_datadir='./', 
                  thres=0.3, nx=128, ny=128, nbands=6,
-                 compute_type='xxx', global_mean=np.zeros((6))):
+                 compute_type='xxx', 
+                 global_mean=np.zeros((6)), 
+                 tf_filelists=[] ):
   """
     global_mean = [# of bands, ]
   """
-  clouds_patches_list = []
-  for m2_file in m2_filelist:
-    # get hdf file
-    hdf_m2 = SD(m2_file, SDC.READ)
-    m35_file = get_filepath(m2_file,mod35_datadir, prefix='MOD35_L2.A')
+  if tf_filelists:
+    """
+    lines in case load already copmuted clouds_patches_list and clouds_xy_list
+    from tfrecord
+    
+    tf_filelists is list of tfrecord s.t. [xxx.tfrecord, yyyy.tfreocrd, ...]
+    """  
+    # get number of patches
+    npatches=0
+    for tfrecord in tf_filelists:
+      npatches += _get_num_imgs(tfrecord)
+    
+    print(" ### Rank %d | Process %d of patches for computatoin ###"
+             % ( MPI.COMM_WORLD.Get_rank() , npatches), flush=True )
+    
+    shape = (nx, ny, nbands)
+    dataset = load_tfrecord(
+                tf_filelists,
+                shape,
+                batch_size=1,
+    )
+    _clouds_patches_list = _get_imgs(dataset, n=npatches)
+    clouds_patches_list = _clouds_patches_list[0]
+    # _clouds_patches_list[0]: np.array data
+    # _clouds_patches_list[1]: np.array shapes?
+    # _clouds_patches_list[2]: original hdf filelist with abspath
+    
+    # collect garbages and save memory
+    gc.collect()
+    
+    # compute against all data
+    all_list = []
+    for ipatch in clouds_patches_list:
+      all_list.append(ipatch.reshape(nx*ny, nbands))
+    alls = np.concatenate(clouds_patches_list, axis=0) 
+    #alls  [# of patches][128*128, 6]
 
-    # get m2 and cloud mask image process
-    mod02_img = gen_mod02_img(hdf_m2)
-    hdf_m35 = SD(m35_file, SDC.READ)
-    clouds_mask_img = gen_mod35_img(hdf_m35)
+    ndata=int(len(clouds_patches_list)*128*128) # number of data in one band
+    # In general, all band must have same number of data(# of patches by # of pixels/patch )
+    #
+  else:
+    #
+    clouds_patches_list = []
+    for m2_file in m2_filelist:
+      # get hdf file
+      hdf_m2 = SD(m2_file, SDC.READ)
+      m35_file = get_filepath(m2_file,mod35_datadir, prefix='MOD35_L2.A')
 
-    # valid patches
-    mod02_patches = _gen_patches(mod02_img, normalization=False)
-    _clouds_patches_list, _ = const_clouds_array(
-            mod02_patches, clouds_mask_img, thres=thres
-    ) # shape [# of patches][128,128,6]
-    clouds_patches_list.extend(_clouds_patches_list)
+      # get m2 and cloud mask image process
+      mod02_img = gen_mod02_img(hdf_m2)
+      hdf_m35 = SD(m35_file, SDC.READ)
+      clouds_mask_img = gen_mod35_img(hdf_m35)
+
+      # valid patches
+      mod02_patches = _gen_patches(mod02_img, normalization=False)
+      _clouds_patches_list, _ = const_clouds_array(
+              mod02_patches, clouds_mask_img, thres=thres
+      ) # shape [# of patches][128,128,6]
+      clouds_patches_list.extend(_clouds_patches_list)
   
-  # collect garbages and save memory
-  gc.collect()
+    # collect garbages and save memory
+    gc.collect()
   
-  # compute against all data
-  all_list = []
-  for ipatch in clouds_patches_list:
-    all_list.append(ipatch.reshape(nx*ny, nbands))
-  alls = np.concatenate(clouds_patches_list, axis=0) 
-  #alls  [# of patches][128*128, 6]
+    # compute against all data
+    all_list = []
+    for ipatch in clouds_patches_list:
+      all_list.append(ipatch.reshape(nx*ny, nbands))
+    alls = np.concatenate(clouds_patches_list, axis=0) 
+    #alls  [# of patches][128*128, 6]
 
-  ndata=int(len(clouds_patches_list)*128*128) # number of data in one band
-  # In general, all band must have same number of data(# of patches by # of pixels/patch )
+    ndata=int(len(clouds_patches_list)*128*128) # number of data in one band
+    # In general, all band must have same number of data(# of patches by # of pixels/patch )
+
+
   try:
     if compute_type == 'mean':
       sums = np.sum(np.concatenate(alls, axis=0), axis=0)
@@ -141,6 +191,16 @@ def get_args():
         "--mod35_datadir",
         type=str
     )
+    p.add_argument(
+        "--tfrecord_datadir",
+        type=str
+    )
+    p.add_argument(
+        "--operate_single",
+        help="operate global mean & stdv operation by single core/ Debug for No parallelized version",
+        type=int,
+        default=0
+    )
 
     FLAGS = p.parse_args()
     return FLAGS
@@ -149,19 +209,24 @@ if __name__ == "__main__":
 
     # flags
     FLAGS = get_args()
+
     #------------------------------------------------------------------
     # Single version
     #------------------------------------------------------------------
-    true_gmeans, true_gstds = compute_global_single(
+    if FLAGS.operate_single == 1:
+      """
+        operate_single {0,1}: 0; DONOT compute. 1; DO compute
+      """
+      true_gmeans, true_gstds = compute_global_single(
                               FLAGS.mod02_datadir,
                               FLAGS.mod35_datadir
                               )
-    if MPI.COMM_WORLD.Get_rank() == 0:
-      print("Non Parallel: Global Mean : ", true_gmeans, flush=True)
-      print("Non Parallel: Global Stdv : ", true_gstds,  flush=True)
-      print(" Terminate computation for truth ", flush=True)
-      print(" ", flush=True)
-    gc.collect()
+      if MPI.COMM_WORLD.Get_rank() == 0:
+        print("Non Parallel: Global Mean : ", true_gmeans, flush=True)
+        print("Non Parallel: Global Stdv : ", true_gstds,  flush=True)
+        print(" Terminate computation for truth ", flush=True)
+        print(" ", flush=True)
+      gc.collect()
 
     #------------------------------------------------------------------
     # Parallel version
@@ -170,6 +235,7 @@ if __name__ == "__main__":
     size = comm.Get_size()
     rank = comm.Get_rank()
 
+    # get two filelists
     filelists = []
     for i, f in enumerate(glob.glob(FLAGS.mod02_datadir+'/*.hdf')):
         if i % size == rank:
@@ -177,11 +243,22 @@ if __name__ == "__main__":
 
     if not filelists:
         raise ValueError("mod02 directory does not match any files")
+    
+    tf_filelists = []
+    for i, f in enumerate(glob.glob(FLAGS.tfrecord_datadir+'/*.tfrecord')):
+        if i % size == rank:
+            tf_filelists.append(os.path.abspath(f))
+
+    if not filelists:
+      if not tf_filelists:
+        raise ValueError("mod02 directory and tfrecord directory do not match any files")
+        
 
     #
     sums, ndata = compute_local(m2_filelist=filelists, 
                                 mod35_datadir=FLAGS.mod35_datadir,
-                                compute_type='mean'
+                                compute_type='mean',
+                                tf_filelists=tf_filelists
     )
     print("Rank %d done." % rank, flush=True)
 
@@ -203,6 +280,8 @@ if __name__ == "__main__":
       _global_mean = np.asarray(global_sums)
       global_mean = np.sum(_global_mean, axis=0)/np.sum(global_ndata)
       print("Parallel: Global Mean : ", global_mean, flush=True)
+    
+      # save data here
     else: 
       # prep recvbuf TODO: let 6 be a argument as number of bands
       #computed_global_mean = np.empty(6,dtype='float64')
@@ -218,7 +297,8 @@ if __name__ == "__main__":
     res_sums, ndata = compute_local(m2_filelist=filelists, 
                                     mod35_datadir=FLAGS.mod35_datadir,
                                     compute_type='stdv',
-                                    global_mean=global_mean
+                                    global_mean=global_mean,
+                                    tf_filelists=tf_filelists
     )
     
     print("Rank %d done." % rank, flush=True)
