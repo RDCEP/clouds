bc24d1739b36bd6ea3ee5cd4cf1d5d3b88be5304
Katy Koenig <kkoenig810@gmail.com>
2019-08-19 15:21:06 -0500

    pylint edits made

--- src_analysis/work_hdfs/find_invalids.py
+++ src_analysis/work_hdfs/find_invalids.py
@@ -15,13 +15,12 @@ import multiprocessing as mp
 import pandas as pd
 from pyhdf.SD import SD, SDC
 import geolocation as geo
-
-
-hdf_libdir = '/home/koenig1/scratch-midway2/clouds/src_analysis/lib_hdfs' # change here
-sys.path.insert(1, os.path.join(sys.path[0], hdf_libdir))
-#from analysis_lib import _gen_patches
-#from alignment_lib import gen_mod35_img
 import prg_StatsInvPixel as stats
+
+HDF_LIBDIR = '/home/koenig1/scratch-midway2/clouds/src_analysis/lib_hdfs' # change here
+sys.path.insert(1, os.path.join(sys.path[0], HDF_LIBDIR))
+#from analysis_lib import _gen_patches
+
 
 DATES_FILE = 'clustering_invalid_filelists.txt'
 MOD02_DIRECTORY = '/home/koenig1/scratch-midway2/MOD02/clustering'
@@ -50,31 +49,30 @@ def get_invalid_info(dates_file=DATES_FILE, mod02_dir=MOD02_DIRECTORY,
         None (writes csv)
     '''
     # Initializes output csv to be appended later
-    with open(output_file, 'w') as csvfile:
-        outputwriter = csv.writer(csvfile, delimiter=',')
+    with open(output_file, 'w') as output_csv:
+        outputwriter = csv.writer(output_csv, delimiter=',')
         outputwriter.writerow(['filename', 'patch_no', 'inval_pixels'])
-    csvfile.close()
+    output_csv.close()
     # Finds name of desired MOD02 hdf files to be analyzed
-    with open(dates_file, "r") as file:
-        dates = file.readlines()
-    desired_files = dates[0].replace('hdf', 'hdf ').split()
-    for file in desired_files:
+    with open(dates_file, "r") as desired_dates:
+        dates = desired_dates.readlines()
+    desired = dates[0].replace('hdf', 'hdf ').split()
+    for mod02_file in desired:
         # Finds actual MOD02 files
-        mod02_path = glob.glob(f'{mod02_dir}/*/{file}')[0]
-        bname = os.path.basename(file)
+        mod02_path = glob.glob(f'{mod02_dir}/*/{mod02_file}')[0]
+        bname = os.path.basename(mod02_file)
         date = bname[10:22]
         # Finds corresponding MOD35
         mod35_path = glob.glob(f'{mod35_dir}/*/*{date}*.hdf')[0]
         fillvalue_list, mod02_img = stats.gen_mod02_img(mod02_path)
-        hdf_m35 = SD(mod35_path, SDC.READ)
-        clouds_mask_img = stats.gen_mod35_img(hdf_m35)
+        clouds_mask_img = stats.gen_mod35_img(SD(mod35_path, SDC.READ))
         mod02_patches = _gen_patches(mod02_img, normalization=False)
         # Checks validity of pixels for each file and writes csv
-        stats.check_invalid_clouds2(output_file, file, mod02_patches,
+        stats.check_invalid_clouds2(output_file, mod02_file, mod02_patches,
                                     clouds_mask_img, fillvalue_list, thres=0.3)
 
 
-def get_invalid_info2(file, mod02_dir=MOD02_DIRECTORY,
+def get_invalid_info2(filename, mod02_dir=MOD02_DIRECTORY,
                       mod35_dir=MOD35_DIRECTORY, output_file=OUTPUT_CSV):
     '''
     Searches for desired files and writes csv with each row being a MOD02
@@ -95,15 +93,15 @@ def get_invalid_info2(file, mod02_dir=MOD02_DIRECTORY,
         None (writes csv)
     '''
     # Finds actual MOD02 file
-    mod02_glob = glob.glob(f'{mod02_dir}/*/*/{file}')
-    mod02_patches, fillvalue_list = gen_mod02(mod02_glob, file)
+    mod02_glob = glob.glob(f'{mod02_dir}/*/*/{filename}')
+    mod02_patches, fillvalue_list = gen_mod02(mod02_glob, filename)
     # Finds corresponding MOD35
-    date = os.path.basename(file)[10:22]
+    date = os.path.basename(filename)[10:22]
     mod35_glob = glob.glob(f'{mod35_dir}/*/*{date}*.hdf')
     clouds_mask_img = gen_mod35(mod35_glob)
     if clouds_mask_img and mod02_patches:
         # Checks validity of pixels for each file and writes csv
-        stats.check_invalid_clouds2(output_file, file, mod02_patches,
+        stats.check_invalid_clouds2(output_file, filename, mod02_patches,
                                     clouds_mask_img, fillvalue_list, thres=0.3)
 
 
@@ -121,27 +119,28 @@ def get_info_for_location(mod02_dir, mod35_dir, mod03_dir, outputfile, nparts):
 
     Outputs: None (creates and saves a csv file)
     '''
-    with open(outputfile, 'w') as csvfile:
-        outputwriter = csv.writer(csvfile, delimiter=',')
+    with open(outputfile, 'w') as out_csv:
+        outputwriter = csv.writer(out_csv, delimiter=',')
         outputwriter.writerow(['filename', 'patch_no', 'invalid_pixels',
                                'geometry'])
+    out_csv.close()
 
     mod02_files = glob.glob(f'{mod02_dir}/*/*/*/*.hdf')
     for mod02_file in mod02_files:
-        print(mod02_file)
         file_base = mod02_file[-34:-22]
         location_date = mod02_file[50:70]
         mod35_path = glob.glob(f'{mod35_dir}/*/{location_date}/*{file_base}*.hdf')
         cloud_mask_img = gen_mod35(mod35_path, file_base)
         mod03_path = glob.glob(f'{mod03_dir}/*/{location_date}/*{file_base}*.hdf')
         latitude, longitude = gen_mod03(mod03_path, file_base)
-        patches, fill_values, lats, longs = gen_mod02(mod02_file, file_base, latitude,
-                                                         longitude)
-        geo.connect_geolocation(f'{file_base}_{location_date}', outputfile, patches, fill_values, lats,
-                                longs, cloud_mask_img, nparts)
+        patches, fill_values, lats, longs = gen_mod02(mod02_file, file_base,
+                                                      latitude, longitude)
+        geo.connect_geolocation(f'{file_base}_{location_date}', outputfile,
+                                patches, fill_values, lats, longs,
+                                cloud_mask_img, nparts)
 
 
-def gen_mod02(mod02_file, file, latitude=None, longitude=None):
+def gen_mod02(mod02_file, filename, latitude=None, longitude=None):
     '''
     Generates the mod02 patches and the fill value list for a MOD02 hdf file
 
@@ -154,10 +153,10 @@ def gen_mod02(mod02_file, file, latitude=None, longitude=None):
         mod02_patches: numpy array of arrays representing MOD02 patches
         fillvalue_list: list of integers for each fill value
     '''
-    if not mod02_file: 
-        print("No mod02 file downloaded for " + file)
+    if not mod02_file:
+        print(f"No mod02 file downloaded for {filename}")
         return None
-    elif len(mod02_file) == 1:
+    if len(mod02_file) == 1:
         mod02_file = mod02_file[0]
     return geo.make_patches(mod02_file, latitude, longitude)
 
@@ -178,7 +177,7 @@ def gen_mod35(mod35_file, date=None):
     if not mod35_file:
         print(f"No mod35 file downloaded for {date}")
         return None
-    elif len(mod35_file) == 1:
+    if len(mod35_file) == 1:
         mod35_file = mod35_file[0]
     hdf_m35 = SD(mod35_file, SDC.READ)
     clouds_mask_img = stats.gen_mod35_img(hdf_m35)
@@ -202,7 +201,7 @@ def gen_mod03(mod03_file, date):
     if not mod03_file:
         print(f"No mod03 file downloaded for {date}")
         return None
-    elif len(mod03_file) == 1:
+    if len(mod03_file) == 1:
         mod03_file = mod03_file[0]
     mod03_hdf = SD(mod03_file, SDC.READ)
     lat = mod03_hdf.select('Latitude')
@@ -213,40 +212,39 @@ def gen_mod03(mod03_file, date):
 
 
 if __name__ == "__main__":
-    p = argparse.ArgumentParser()
-    p.add_argument('--dates_file', type=str, default=DATES_FILE)
-    p.add_argument('--processors', type=int, default=5)
-    p.add_argument('--outputfile', type=str, default=OUTPUT_CSV)
-    args = p.parse_args()
-    print("Num. Processors: " + args.processors)
+    P = argparse.ArgumentParser()
+    P.add_argument('--dates_file', type=str, default=DATES_FILE)
+    P.add_argument('--processors', type=int, default=5)
+    P.add_argument('--outputfile', type=str, default=OUTPUT_CSV)
+    ARGS = P.parse_args()
+    print(f"Num. Processors: {ARGS.processors}")
     #Initializes pooling process for parallelization
-    pool = mp.Pool(processes=args.processors)
+    POOL = mp.Pool(processes=ARGS.processors)
     # If output csv exits, assumes cutoff by RCC so deletes last entry
     # and appends only new dates
-    if os.path.exists(args.outputfile):
+    if os.path.exists(ARGS.outputfile):
         print('Checking for completion')
-        completed = pd.read_csv(args.outputfile)
-        completed = completed[completed['filename'].notnull()]
-        last_file = completed.tail(1)['filename'].tolist()[0]
-        done = completed[completed['filename'] != last_file]
-        print('Writing updated csv w/ only completed MOD02 files')
-        done.to_csv(args.outputfile, index=False)
+        COMPLETED = pd.read_csv(ARGS.outputfile)
+        COMPLETED = COMPLETED[COMPLETED['filename'].notnull()]
+        LAST_FILE = COMPLETED.tail(1)['filename'].tolist()[0]
+        DONE = COMPLETED[COMPLETED['filename'] != LAST_FILE]
+        print('Writing updated csv w/ only COMPLETED MOD02 files')
+        DONE.to_csv(ARGS.outputfile, index=False)
     else:
         # Initializes output csv to be appended later
-        with open(args.outputfile, 'w') as csvfile:
-            outputwriter = csv.writer(csvfile, delimiter=',')
-            outputwriter.writerow(['filename', 'patch_no', 'inval_pixels'])
+        with open(ARGS.outputfile, 'w') as csvfile:
+            OUTPUTWRITER = csv.writer(csvfile, delimiter=',')
+            OUTPUTWRITER.writerow(['filename', 'patch_no', 'inval_pixels'])
         csvfile.close()
-        last_file = None
+        LAST_FILE = None
     # Finds name of desired MOD02 hdf files to be analyzed
-    with open(args.dates_file, "r") as file:
-        dates = file.readlines()
-    desired_files = dates[0].replace('hdf', 'hdf ').split()
-    if last_file:
-        last_idx = desired_files.index(last_file)
-        desired_files = desired_files[last_idx:]
+    with open(ARGS.dates_file, "r") as file:
+        DATES = file.readlines()
+    DESIRED_FILES = DATES[0].replace('hdf', 'hdf ').split()
+    if LAST_FILE:
+        LAST_IDX = DESIRED_FILES.index(LAST_FILE)
+        DESIRED_FILES = DESIRED_FILES[LAST_IDX:]
 
-    pool.map(get_invalid_info2, desired_files)
-    pool.close()
-    pool.join()
-
+    POOL.map(get_invalid_info2, DESIRED_FILES)
+    POOL.close()
+    POOL.join()
