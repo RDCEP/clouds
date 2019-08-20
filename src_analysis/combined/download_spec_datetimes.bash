dcd4c2b6649c18de187508b49e3d71a1202b47d9
Katy Koenig <kkoenig810@gmail.com>
2019-08-20 16:22:12 -0500

    adding example bash scripts

---
+++ src_analysis/combined/download_spec_datetimes.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+python3 prg_geturl2.py --download_process='download_from_name' \
+    --outputdir=$(pwd) \
+    --processors=7 \
+    --keyword='MOD35' \
+    --input_csv="/scratch/midway2/koenig1/clouds/src_analysis/work_hdfs/" + \
+    			"datetime_example.csv.csv"
