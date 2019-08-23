83dfa33d22745767d4cf21756fc59a90687657f5
Katy Koenig <kkoenig810@gmail.com>
2019-08-23 11:12:03 -0500

    edited fi bash scripts

--- src_analysis/work_hdfs/fi_get_info_for_location.bash
+++ src_analysis/work_hdfs/fi_get_info_for_location.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 prg_geturl2.py --process='get_info_for_location' \
+python3 find_invalids.py --process='get_info_for_location' \
     --mod02dir='/home/koenig1/scratch-midway2/MOD021KM' \
     --mod35_dir='/home/koenig1/scratch-midway2/MOD35_L2' \
     --mod03_dir='/home/koenig1/scratch-midway2/MOD03' \
--- src_analysis/work_hdfs/fi_get_invalid_info.bash
+++ src_analysis/work_hdfs/fi_get_invalid_info.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 prg_geturl2.py --process='get_invalid_info' \
+python3 find_invalids.py --process='get_invalid_info' \
 	--input='clustering_invalid_filelists.txt' \
     --mod02dir='/home/koenig1/scratch-midway2/MOD021KM' \
     --mod35_dir='/home/koenig1/scratch-midway2/MOD35_L2' \
