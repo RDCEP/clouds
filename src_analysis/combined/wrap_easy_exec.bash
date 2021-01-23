d2ece808bb5fd0374fa6baf59508ce09226c7017
TakGlobus <tkurihana@uchicago.edu>
2021-01-22 20:31:53 -0600

    a

--- src_analysis/combined/wrap_easy_exec.bash
+++ src_analysis/combined/wrap_easy_exec.bash
@@ -2,17 +2,26 @@
 
 appkey='210B0BEC-13E1-11EA-B059-AE780D77E571'
 
-BASEDIR="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD021KM/2008"
-outputdir="/home/tkurihana/Research/data/MYD02"
+BASEDIR="https://ladsweb.modaps.eosdis.nasa.gov/archive/allData/61/MYD021KM"
+#outputdir="/home/tkurihana/Research/data/MYD02"
+outputdir="/home/tkurihana/scratch-midway2/data/MYD02"
 
-dates_list=(
-  '053' '068' '121' '122' '168' '189' '235' '272' '282' '312' '337'
-)
+#dates_list=(
+#)
 
 
-for idate in "${dates_list[@]}" ; do
-  echo $idate
+#for idate in "${dates_list[@]}" ; do
+#  echo $idate
+#  inputdir=$BASEDIR/$iyear/$idate/
+#  bash easy_execute.bash $inputdir $appkey $outputdir
+#done
 
-  inputdir=$BASEDIR/$idate/
+while read line
+do 
+  iyear=`echo ${line:0:4}`
+  idate=`echo ${line:4}`
+  echo $iyear $idate
+
+  inputdir=$BASEDIR/$iyear/$idate/
   bash easy_execute.bash $inputdir $appkey $outputdir
-done
+done  < ./ocean_train.txt
