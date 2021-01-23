d718b216fb1b09e1abc4b34b51314a1b79296663
TakGlobus <tkurihana@uchicago.edu>
2021-01-23 15:49:15 -0600

    update training patch creation code

--- reproduction/pipeline/into_mod_update_record.py
+++ reproduction/pipeline/into_mod_update_record.py
@@ -37,8 +37,10 @@ clouds_dir="/Research/clouds/src_analysis/lib_hdfs"
 #clouds_dir="/clouds/src_analysis/lib_hdfs"
 sys.path.insert(1,os.path.join(sys.path[0],homedir+clouds_dir))
 from alignment_lib import gen_mod35_img
+from alignment_lib import gen_mod35_ocean_img
 from alignment_lib import get_filepath
 from alignment_lib import translate_const_clouds_array
+from alignment_lib import translate_const_ocean_array
 
 def _int64_feature(value):
     return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
@@ -150,7 +152,7 @@ def gen_sds(filelist=[], ref_var='EV_500_Aggr1km_RefSB', ems_var='EV_1KM_Emissiv
     yield ifile, swath
 
 
-def gen_patches(swaths, stride=64, patch_size=128, 
+def gen_patches_old(swaths, stride=64, patch_size=128, 
                  normalization=False, flag_shuffle=False,
                  global_mean=np.zeros((6)), global_stdv=np.ones((6)),
                  mod35_datadir='./', thres=0.3, prefix='MOD35_L2.A'):
@@ -258,6 +260,133 @@ def gen_patches(swaths, stride=64, patch_size=128,
               #    clouds_patch[:,:,ichannel] = np.log10(clouds_patch[:,:,ichannel]+1.0e-10)
 
               yield fname, (i, j), clouds_patch
+
+def gen_patches(swaths, stride=64, patch_size=128, 
+                 normalization=False, flag_shuffle=False,
+                 global_mean=np.zeros((6)), global_stdv=np.ones((6)),
+                 mod35_datadir='./', thres=0.3, ocean_thres=0.999,
+                 apply_ocean_flag=True, 
+                 prefix='MOD35_L2.A'):
+    
+    """Normalizes swaths and yields patches of size `shape` every `strides` pixels
+        IN:  swath;   image data in hdf file
+             stride;  # patch stride size. size/2 is defualt
+             size;    # patch in x/y direction  
+             normalization; z-score normalization. For MODIS data, MUST False
+             flag_nan; # detect option for np.nan information
+             flag_shuffle; shuffle data or not
+
+        * Document 
+        For Decoded SDS MODIS Dataset, User should NOT normalize the input data
+        because z-score normalization mess up radiance infromation
+
+        IF user get patches WITH Normalization
+         normalization = True
+         Otherwise, vals in patch are NOT normalized
+        IF user want to get patches WITHOUT NAN value
+         flag_nan=True
+         Then, nanvalue will be excluded
+
+        UPDATE: 2019/07/25
+          To normalize input correctly, user apply normalization scheme with
+          computed global_mean and global_stdv
+          For usage, turn normalization True and give global_mean and global_stdv
+
+    * Generic document by Casper
+    Args:
+        swaths: Iterable of (filename, np.ndarray) to slice patches from
+        shape: (height, width) patch size
+        strides: (x_steps, y_steps) how many pixels between patches
+    Yields:
+        (filename, coordinate, patch): where the coordinate is the pixel coordinate of the
+        patch inside of filename. BUG: pixel coorindate is miscalculated if swath is
+        resized. Patches come from the swath in random order and are whiten-normalized.
+    """
+    # checkio for normalizaion scheme
+    if normalization:
+        norm_ok_flag = 0
+        # check initial array are replaced by parsed computed array
+        if not global_mean.all() == 0.000:
+          norm_ok_flag += 1
+          if not global_stdv.all() == 1.000 :
+            norm_ok_flag += 1
+        try:
+          if norm_ok_flag == 2:
+             print(" Apply Normalization scheme by global mean&stdv ", flush=True)
+        except:
+          raise ValueError(" Correct global mean & stdv are not parsed")
+            
+      
+    # params
+    for fname, swath in swaths:
+
+      print(fname, flush=True)
+      #### TODO Visualization for Aqua band 6 -- striping issue 
+      #np.save(f"./swaths/{os.path.basename(fname)}.npy",swath)
+      max_x, max_y, _ = swath.shape
+
+      # Shuffle patches
+      coords = []
+      for x in range(0, max_x, stride):
+         for y in range(0, max_y, stride):
+           if x + patch_size < max_x and y + patch_size < max_y:
+              coords.append((x, y))
+      # Baseically OFF
+      #if flag_shuffle:
+      #  np.random.shuffle(coords)
+      
+      # Get MOD35 data
+      m35_file = get_filepath(fname, mod35_datadir, prefix=prefix)
+      hdf_m35 = SD(m35_file, SDC.READ)
+      if hdf_m35 is None:
+        # call mpiabort function
+        sys.excepthook =  mpiabort_excepthook
+        sys.excepthook = sys.__excepthook__  # 2nd call?
+      #if hdf_m35 is None:
+      #  print(" Program Forcibly Terminate: Rank %d" % MPI.COMM_WORLD.Get_rank(), flush=True)
+      #  MPI.COMM_WORLD.Abort()
+      clouds_mask_img = gen_mod35_img(hdf_m35)
+      print("GEN. CLOUD MASK", flush=True)
+
+      # 2021/01/12 generate ocean mask
+      ocean_mask_img = gen_mod35_ocean_img(hdf_m35) 
+      print("GEN. OCEAN MASK", flush=True)
+
+
+      for i, j in coords:
+        patch = swath[i:i + patch_size, j:j + patch_size]
+
+        #if normalization:
+        #  patch -= global_mean
+        #  patch /= global_stdv
+        
+        if not np.isnan(patch).any():
+          #TODO: Add lines below to compare MOD35
+          # translate_const_clouds_array is based on const_clouds_array
+          clouds_patch, clouds_flag = translate_const_clouds_array(
+              patch, clouds_mask_img, thres=thres, coord=(i,j)
+          )
+          if clouds_flag:
+            if not patch is None:
+
+              # Apply log transform to skew data
+              #channels = clouds_patch.shape[2] # 128,128,6
+              #for ichannel in range(channels):
+              #  if ichannel >= 3: #6 channels for band 6,7,20
+              #    clouds_patch[:,:,ichannel] = np.log10(clouds_patch[:,:,ichannel]+1.0e-10)
+
+              if apply_ocean_flag:
+                ocean_clouds_patch, ocean_flag = translate_const_ocean_array(
+                  clouds_patch, ocean_mask_img, thres=ocean_thres, coord=(i,j)
+                )
+
+                if  ocean_flag:
+                  yield fname, (i, j), ocean_clouds_patch
+
+              else :
+                yield fname, (i, j), clouds_patch
+            
+
             
 
 def write_feature(writer, filename, coord, patch):
@@ -349,11 +478,18 @@ def get_args(verbose=False):
         default=0.3
     )
     p.add_argument(
+        "--thres_ocean_frac", 
+        type=float,
+        help='threshold value range[0-1) for alignment process',
+        default=0.999
+    )
+    p.add_argument(
         "--prefix", 
         type=str,
         help='basename of MOD35 or MYD35 name e.g. MYD35_L2.A2 ',
         default=0.3
     )
+    p.add_argument('--ocean_flag', action='store_true')
     
     # parse only one band info by int parser
     #p.add_argument(
@@ -424,7 +560,8 @@ if __name__ == "__main__":
     #TODO modify arg to add multiple temp/altitude bands  
     swaths  = gen_sds(fnames, 
                       ref_var='EV_500_Aggr1km_RefSB', ems_var='EV_1KM_Emissive',
-                      ref_bands=["4","7"], ems_bands=FLAGS.ems_band) # Aqua: replace band 6 with band 7
+                      ref_bands=["5","7"], ems_bands=FLAGS.ems_band) # Aqua: replace band 6 with band 7
+                      #ref_bands=["4","7"], ems_bands=FLAGS.ems_band) # Aqua: replace band 6 with band 7
                       #ref_bands=["6","7"], ems_bands=FLAGS.ems_band) # Terra
                       #ref_bands=["6","7"], ems_bands=["20"]+FLAGS.ems_band)
     
@@ -433,7 +570,10 @@ if __name__ == "__main__":
                           global_mean=global_mean,
                           global_stdv=global_stdv,
                           mod35_datadir=FLAGS.mod35_datadir, 
-                          thres=FLAGS.thres_cloud_frac,prefix=FLAGS.prefix )
+                          thres=FLAGS.thres_cloud_frac,prefix=FLAGS.prefix,
+                          ocean_thres=FLAGS.thres_ocean_frac,
+                          apply_ocean_flag=FLAGS.ocean_flag, 
+                          )
 
     write_patches(patches, FLAGS.out_dir, FLAGS.patches_per_record)
 
